5690e1d1e562d9b2ad68fef170f72fb41beab40d
woroszek <woroszek@gmail.com>
2023-09-26 18:41:30 +0200

    Dodano poprawki prinow

--- zadaniedomowe.py
+++ zadaniedomowe.py
@@ -12,7 +12,6 @@ for i in range(a):
             taba.append(20 - waga_paczki)
             break
         else:
-            i += 1
             waga_ogolna += waga_ost_elem
             break
     waga_ogolna += waga_ost_elem
@@ -26,17 +25,19 @@ for i in range(a):
         licznik_paczek += 1
         taba.append(20 - waga_paczki)
     
-        
-    
-
 waga_pusta = licznik_paczek * 20 - waga_ogolna
+
 if licznik_paczek > 1:
     print(f'Wysłano {licznik_paczek} paczki o łącznej masie {waga_ogolna} kg.')
-    print(f'Ogólna liczba pustych kilogramów w wysłanych paczkach to: {waga_pusta} kg.')
-    print(f'Najwięcej pustych kilogramów miała paczka nr. {(taba.index(max(taba))+1)}: {max(taba)} pustych kilogramów.')
+    
 elif licznik_paczek == 1:
     print(f'Wysłano {licznik_paczek} paczkę o łącznej masie {waga_ogolna} kg.')
+    
+else:
+    print("Nie wysłano żadnej paczki.")
+    
+if waga_pusta > 0:
     print(f'Ogólna liczba pustych kilogramów w wysłanych paczkach to: {waga_pusta} kg.')
     print(f'Najwięcej pustych kilogramów miała paczka nr. {(taba.index(max(taba))+1)}: {max(taba)} pustych kilogramów.')
 else:
-    print("Nie wysłano żadnej paczki")
+    print("Brak pustych kilogramów")
